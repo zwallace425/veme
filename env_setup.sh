ee64c87b440c666a23fd6e6b187ef12eb6aadd77
zwallace425 <zwallace@jcvi.org>
2023-07-24 12:25:47 -0700

    env setup

---
+++ env_setup.sh
@@ -0,0 +1,10 @@
+pip install pandas
+pip install numpy
+pip install umap-learn
+pip install distinctipy
+pip install -U scikit-learn
+pip install -U matplotlib
+pip install scikit-network
+pip install scanpy
+pip install anndata
+pip install --user nsforest
